ea23bd4b828f4354798eeb828e2d25c2ab85b7c0
Skylar Thomas <skytblue@gmail.com>
2022-10-26 20:42:47 -0700

    all prefab saving works

--- Assets/Playbook/Scripts/ExportManager.cs
+++ Assets/Playbook/Scripts/ExportManager.cs
@@ -5,7 +5,6 @@ using System.Collections;
 
 public class ExportManager : MonoBehaviour
 {
-    public Texture2D s;
     public enum Setting
     {
         Selected,
@@ -27,49 +26,6 @@ public class ExportManager : MonoBehaviour
         AssetDatabase.Refresh();
     }
 
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-            Debug.Log("Testing!");
-
-            GameObject[] objectArray = GameObject.FindGameObjectsWithTag("Exportable");
-            int count = 0;
-
-            foreach (GameObject g in objectArray)
-            {
-                Texture2D itemBGTex = objectArray[0].GetComponentInChildren<Renderer>().material.mainTexture as Texture2D;
-                itemBGTex.alphaIsTransparency = true;
-                byte[] itemBGBytes = itemBGTex.EncodeToPNG();
-                string texPath = "Assets/Resources/" + "Texture_" + count + ".png";
-                File.WriteAllBytes(texPath, itemBGBytes);
-
-                AssetDatabase.ImportAsset(texPath);
-                AssetDatabase.SaveAssets();
-                AssetDatabase.Refresh();
-
-                //------------
-
-                Material mat = new Material(Shader.Find("Universal Render Pipeline/Unlit"));
-                mat.SetFloat("_Surface", 1.0f);
-                mat.renderQueue = 2999;
-
-                var texture = Resources.Load<Texture2D>("Texture_" + count);
-                Debug.Log(texture);
-                mat.mainTexture = texture;
-
-                string matPath = "Assets/Exported/" + "Material_" + count + ".mat";
-                AssetDatabase.CreateAsset(mat, matPath);
-                AssetDatabase.SaveAssets();
-                AssetDatabase.Refresh();
-
-                count++;
-            }
-
-        }
-
-    }
-
     public void SavePrefabs()
     {
         GameObject[] objectArray = GameObject.FindGameObjectsWithTag("Exportable");
@@ -78,38 +34,43 @@ public class ExportManager : MonoBehaviour
 
         //Assign all to root parent
         GameObject p = new GameObject("FigmaElements");
+        int count = 0;
 
         foreach (GameObject g in objectArray)
         {
             g.transform.parent = p.transform;
 
             Texture2D itemBGTex = g.GetComponentInChildren<Renderer>().material.mainTexture as Texture2D;
-            //Texture2D itemBGTex = s;
             itemBGTex.alphaIsTransparency = true;
             byte[] itemBGBytes = itemBGTex.EncodeToPNG();
-            string texPath = "Assets/Exported/" + "Texture_" + objectCount + ".png";
+            string texPath = "Assets/Resources/" + "Texture_" + count + ".png";
             File.WriteAllBytes(texPath, itemBGBytes);
 
+            AssetDatabase.ImportAsset(texPath);
             AssetDatabase.SaveAssets();
             AssetDatabase.Refresh();
+
+            //------------
 
             Material mat = new Material(Shader.Find("Universal Render Pipeline/Unlit"));
             mat.SetFloat("_Surface", 1.0f);
             mat.renderQueue = 2999;
-           // mat.mainTexture = LoadPNG(texPath);
-            string matPath = "Assets/Exported/" + "Material_" + objectCount + ".mat";
-            AssetDatabase.CreateAsset(mat, matPath);
+            var texture = Resources.Load<Texture2D>("Texture_" + count);
+            Debug.Log(texture);
+            mat.mainTexture = texture;
 
+            g.GetComponentInChildren<Renderer>().material = mat;
+
+            string matPath = "Assets/Exported/" + "Material_" + count + ".mat";
+            AssetDatabase.CreateAsset(mat, matPath);
             AssetDatabase.SaveAssets();
             AssetDatabase.Refresh();
 
-            g.GetComponentInChildren<Renderer>().material = mat;
-            objectCount++;
-
+            count++;
         }
 
         // Save grouped assets as prefab if assets have been imported
-        if(objectArray.Length > 0)
+        if (objectArray.Length > 0)
         {
             if (!Directory.Exists("Assets/Exported"))
                 AssetDatabase.CreateFolder("Assets", "Exported");
